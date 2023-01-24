48fb32307188a4d73dd3da793c0278f4e6cf099b
PauloVFC94 <paulovictorfcosta@gmail.com>
2023-01-24 17:47:52 -0300

    Anotações sobre o console.log, cometários e sobre funcionamento do JS

--- dia1/aula1.js
+++ dia1/aula1.js
@@ -1,6 +1,10 @@
-// comando de saída//
+// comando de saída
 console.log("Olá, mundo!");
 
-//no terminal posso usar o comando node + o nome do arquivo para executá-lo Ex: node aula1.js//
-//no vscode já existe a opção de rodar diretamente através da IDE//
+//Para fazer comentários utilizar "//" para comentar em linha
+// para comentar em bloco utilizar "/*" e fechar com "*/"
 
+//no terminal posso usar o comando node + o nome do arquivo para executá-lo Ex: node aula1.js
+//no vscode já existe a opção de rodar diretamente através da IDE
+
+//Js é uma linguagem interpretada, ou seja não necessita de um "exe" para ser executado
