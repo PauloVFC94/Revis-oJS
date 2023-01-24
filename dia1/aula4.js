b9a467263d9941f5d7c365a29106b61651164140
PauloVFC94 <paulovictorfcosta@gmail.com>
2023-01-24 18:48:13 -0300

    Operadores matemáticos no JS

---
+++ dia1/aula4.js
@@ -0,0 +1,46 @@
+//Operadores matemáticos
+
+let numero1 = 10;
+let numero2 = 20;
+
+//soma
+
+let soma = numero1 + numero2;
+console.log(soma); //saída -> 30
+
+//subtração
+
+let subtracao = numero1 - numero2;
+console.log(subtracao); //saída -> -10
+
+//multiplicacao
+
+let multiplicacao = numero1 * numero2;
+console.log(multiplicacao); //saída -> 200
+
+//divisao
+
+let divisao = numero1 / numero2;
+console.log(divisao); //saída -> 0.5
+
+//resto
+
+let resto = numero2 % numero1;
+console.log(resto); //saída -> 0
+
+//operador de incremento
+
+numero1++;
+console.log(numero1); //saída -> 11;
+
+numero1 += 2;
+console.log(numero1); //saída -> 13;
+
+//operador de decréscimo
+
+numero1--;
+console.log(numero1); //saída -> 12;
+
+numero1 -= 2;
+console.log(numero1); //saída -> 10;
+
