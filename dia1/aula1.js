3ca86b863e1c464f6e3f940c99c4670fa353e1b4
PauloVFC94 <paulovictorfcosta@gmail.com>
2023-01-24 16:28:57 -0300

    Aula 01 - Revisão Javascript - Console.log

---
+++ dia1/aula1.js
@@ -0,0 +1,6 @@
+// comando de saída//
+console.log("Olá, mundo!");
+
+//no terminal posso usar o comando node + o nome do arquivo para executá-lo Ex: node aula1.js//
+//no vscode já existe a opção de rodar diretamente através da IDE//
+
